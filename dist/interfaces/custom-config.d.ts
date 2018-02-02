02c4736c6fdf1aece466701cc39bb97f343d58b0
oleg.rybnikov <oleg.rybnikov@valor-software.com>
2018-02-02 15:02:10 +0200

    move hystrix as dependency

--- dist/interfaces/custom-config.d.ts
+++ dist/interfaces/custom-config.d.ts
@@ -1,6 +1,3 @@
 export interface CustomConfig {
     circuitBreakerNotificationEmail: string;
 }
-export declare class CCser {
-    age: number;
-}
--- dist/interfaces/custom-config.js
+++ dist/interfaces/custom-config.js
@@ -1,9 +1,3 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class CCser {
-    constructor() {
-        this.age = 103330022;
-    }
-}
-exports.CCser = CCser;
 //# sourceMappingURL=custom-config.js.map
